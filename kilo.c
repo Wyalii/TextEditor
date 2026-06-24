af087ffc07e982f6f30df7d539c76a7494f949a3
Wyalii <gigaurigiorgi2727@gmail.com>
2026-06-24 23:19:57 +0400

    there is lots of termios settings that we use i need to learn that library/package deep first.

--- kilo.c
+++ kilo.c
@@ -13,23 +13,31 @@ void enableRawMode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(ICRNL | IXON | BRKINT | ISTRIP | INPCK);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main()
 {
     enableRawMode();
-    char c;
+    char c = '\0';
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     {
         if (iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q')
+            break;
     }
     return 0;
 }
