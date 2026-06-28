241a51c1cfd6a3fcd09403715d6329ce92a3a996
Wyalii <gigaurigiorgi2727@gmail.com>
2026-06-28 16:05:26 +0400

    focused on learning theory on OS. i need better understanding of lower level world.

--- tinkering/termios.c
+++ tinkering/termios.c
@@ -1,14 +1,8 @@
-#include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
-int main()
+int main(void)
 {
-    struct termios t;
-    tcgetattr(STDIN_FILENO, &t);
-
-    printf("c_lflag: %lu\n", t.c_lflag);
-    printf("c_iflag: %lu\n", t.c_iflag);
-    printf("c_oflag: %lu\n", t.c_oflag);
-    printf("c_cflag: %lu\n", t.c_cflag);
-    return 0;
+    // STDIN_FILENO = 0;
+    // STDOUT_FILENO = 1;
+    // STDERR_FILENO = 2
+    write(1, "hello??", 7);
 }
